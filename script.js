58a024ade0097eb6469773f1648a325fa0efd6e4
SmileNattakit <nattakit.rattanakeha@gmail.com>
2024-05-13 14:20:48 +0700

    finish assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+const products = {
+  product_id: 1,
+  produc_name: "ยาสีฟัน",
+  unit_price: 45,
+  quantity_in_stock: 10,
+  product_description: {
+    product_brand: "คอร์เกต",
+    size_or_quantity: "100ml",
+    taste_or_scent: "มินต์",
+    color: "ขาว",
+    addtional_description: "เป็นยาสีฟัน",
+  },
+};
+
+console.log(products);
